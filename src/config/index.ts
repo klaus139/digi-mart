48a957197eed7e8a0af8ae0a772e2f119c0d0daf
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-12-03 17:33:23 +0100

    1.04.16

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        Value: "ui_kits" as const,
+        featured: [
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: "/nav/ui-kits/mixed.jpg"
+            },
+            {
+                name: "New arivals",
+                href: "#",
+                imageSrc: "/nav/ui-kits/blue.jpg"
+            },
+            {
+                name: "Best Sellers",
+                href: "#",
+                imageSrc: "/nav/ui-kits/purple.jpg"
+            }
+        ]
+    },
+    {
+        label: "Icons",
+        Value: "icons" as const,
+        featured: [
+            {
+                name: "Favorite Icon Picks",
+                href: "#",
+                imageSrc: "/nav/icons/picks.jpg"
+            },
+            {
+                name: "New arivals",
+                href: "#",
+                imageSrc: "/nav/icons/new.jpg"
+            },
+            {
+                name: "Bestselling Icons",
+                href: "#",
+                imageSrc: "/nav/icons/bestsellers.jpg"
+            }
+        ]
+    },
+]
