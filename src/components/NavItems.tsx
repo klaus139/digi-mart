ac5bf15197b08080b994399b396e5a821fe0bddc
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-12-04 15:55:52 +0100

    done the cart

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,13 +1,35 @@
 "use client"
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState< null | number >(null);
+    const isAnyOpen = activeIndex !== null
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if(e.key == 'Escape'){
+                setActiveIndex(null)
+            }
+
+        }
+        document.addEventListener("keydown", handler)
+
+        return () => {
+            document.removeEventListener('keydown', handler)
+        }
+
+    }, [])
+
+
+    const navRef = useRef<HTMLDivElement | null>(null);
+
+    useOnClickOutside(navRef, () => setActiveIndex(null))
   return (
-    <div className='flex gap-4 h-full'>
+    <div className='flex gap-4 h-full' ref={navRef}>
         {PRODUCT_CATEGORIES.map((category, i) => {
             const handleOpen = () => {
                 if(activeIndex === i){
@@ -19,7 +41,7 @@ const NavItems = () => {
             const isOpen = i === activeIndex
 
             return (
-                <NavItem />
+                <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen} key={category.value} isAnyOpen={isAnyOpen}/>
             )
         })}
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const PRODUCT_CATEGORIES = [
     {
         label: "UI Kits",
-        Value: "ui_kits" as const,
+        value: "ui_kits" as const,
         featured: [
             {
                 name: "Editor picks",
@@ -22,7 +22,7 @@ export const PRODUCT_CATEGORIES = [
     },
     {
         label: "Icons",
-        Value: "icons" as const,
+        value: "icons" as const,
         featured: [
             {
                 name: "Favorite Icon Picks",
