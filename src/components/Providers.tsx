493586e46c4ab136c86b2b59f94035434be86480
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-12-08 18:31:44 +0100

    new

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,18 +1,32 @@
-"use client"
-import { trpc } from '@/trpc/client';
-import {QueryClient} from '@tanstack/react-query';
-import { httpBatchLink } from '@trpc/client';
-import {useState} from 'react';
+"use client";
+import { trpc } from "@/trpc/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { PropsWithChildren, useState } from "react";
 
-const Providers = () => {
-    const [queryClient] = useState(() => new QueryClient())
-    const [trpcClient] = useState(() => trpc.createClient({
-        links:[
-            httpBatchLink({
-                url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`
-            })
-        ]
-    }))
-}
+const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
+          fetch(url, options) {
+            return fetch(url, {
+              ...options,
+              credentials: "include",
+            });
+          },
+        }),
+      ],
+    })
+  );
 
-export default Providers
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
+
+export default Providers;
